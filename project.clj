2b8bc458127286b87b4104398a8558e51409ba87
Nicolas Berger <nicoberger@gmail.com>
2014-05-18 13:18:06 -0300

    Move rapipago_scraper as a global dependency

--- project.clj
+++ project.clj
@@ -7,10 +7,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-json "0.3.1"]
-                 [compojure "1.1.8"]]
+                 [compojure "1.1.8"]
+                 [rapipago_scraper "0.1.0"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler rapipago_api.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [rapipago_scraper "0.1.0"]
                         [ring-mock "0.1.5"]]}})
