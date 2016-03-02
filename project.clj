fb910bfc13ee9316eeeacb1f6ff3ae43d19b4cac
Nicolas Berger <nicoberger@gmail.com>
2016-03-02 16:48:58 -0300

    Bump dependencies

--- project.clj
+++ project.clj
@@ -5,17 +5,17 @@
         :url "https://github.com/nberger/rapipago_api"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.cache "0.6.3"]
-                 [ring/ring-json "0.3.1"]
-                 [compojure "1.1.8"]
-                 [ring/ring-jetty-adapter "1.2.1"]
-                 [ring-cors "0.1.4"]
-                 [clojurewerkz/elastisch "2.0.0"]
-                 [rapipago_scraper "0.1.0"]
-                 [com.climate/claypoole "0.2.2"]
-                 [geocoder-clj "0.2.4"]]
-  :plugins [[lein-ring "0.8.10"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.cache "0.6.4"]
+                 [ring/ring-json "0.4.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring-cors "0.1.7"]
+                 [clojurewerkz/elastisch "2.2.1"]
+                 [rapipago_scraper "0.1.1-SNAPSHOT"]
+                 [com.climate/claypoole "1.1.2"]
+                 [geocoder-clj "0.2.5"]]
+  :plugins [[lein-ring "0.9.7"]]
   :ring {:handler rapipago_api.handler/app}
   :main rapipago_api.server
   :min-lein-version "2.4.0"
