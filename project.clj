be9dbd03396f41ec991977233ffde78747c7cbda
Nicolas Berger <nicoberger@gmail.com>
2016-03-02 17:03:37 -0300

    :aot :all for heroku

--- project.clj
+++ project.clj
@@ -22,4 +22,5 @@
   :min-lein-version "2.4.0"
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                        [ring-mock "0.1.5"]]}
+   :uberjar {:aot :all}})
