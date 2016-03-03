4eb105c35f978b610032f3e21d9091c4db6a2785
Nicolas Berger <nicoberger@gmail.com>
2016-03-03 00:31:52 -0300

    Allow importing just 1 province

--- src/rapipago_api/import_stores.clj
+++ src/rapipago_api/import_stores.clj
@@ -2,7 +2,12 @@
   (:require
     [rapipago_api.es-store :as es-store]))
 
-(defn -main [& args]
-  (doseq [province (keys (es-store/provinces es-store/db))]
-    (println "importing province " province)
-    (es-store/refresh-province province 10)))
+(defn import-province [province]
+  (println "importing province " province)
+  (es-store/refresh-province province 10))
+
+(defn -main [[province & more]]
+  (if province
+    (import-province province)
+    (doseq [province (keys (es-store/provinces es-store/db))]
+      (import-province province))))
